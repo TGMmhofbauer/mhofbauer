c6c69d6655ab0594bbd068f86100fb2d3928af6f
TGMmhofbauer <4ff321@gmail.com>
2014-03-19 09:29:19 +0100

    JUnit hinzugefügt.

---
+++ test/TestCipher.java
@@ -0,0 +1,14 @@
+package hofbauer.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCipher {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
