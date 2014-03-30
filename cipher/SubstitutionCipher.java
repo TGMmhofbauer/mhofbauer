bfaccbb71e84a4b14cd625aee944c1c2b408e906
TGMmhofbauer <4ff321@gmail.com>
2014-03-30 20:53:57 +0200

    documentation

--- cipher/SubstitutionCipher.java
+++ cipher/SubstitutionCipher.java
@@ -1,12 +1,34 @@
 package hofbauer.cipher;
 
+import hofbauer.cipher.MonoalphabeticCipher;
+
+/**
+ * Used to en- and decrypt Strings.
+ * 
+ * All chars from a given String are replaced according to the given secret alphabet.
+ * Every index of the String is replaced by the same index but from the secret alphabet.
+ * 
+ * @author Manfred Hofbauer
+ *
+ */
 public class SubstitutionCipher extends MonoalphabeticCipher {
 
-	public SubstitutionCipher(String secretalphabet){
-		
+	/**
+	 * Constructor is setting the secret alphabet to the given secretAlphabet 
+	 *
+	 * @param secretAlphabet the secrecet alphabet which is used to encrypt Strings
+	 */
+	public SubstitutionCipher(String secretAlphabet) {
+		setSecretAlphabet(secretAlphabet);
 	}
 	
-	public void setSecretAlphabet(String secretAlphabet){
-		super.setSecretAlphabet(secretAlphabet);
+	/**
+	 * secret alphabet is set to the given secretAlphabet
+	 * 
+	 * @param alphabet the secrecet alphabet which is used to encrypt Strings
+	 */
+	@Override
+	public void setSecretAlphabet(String alphabet) {
+		super.setSecretAlphabet(alphabet);
 	}
 }
