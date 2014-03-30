cca24c4838ac7f130fef17ad00192004c5f6d31a
TGMmhofbauer <4ff321@gmail.com>
2014-03-30 19:01:21 +0200

    added documentation

--- cipher/Cipher.java
+++ cipher/Cipher.java
@@ -1,8 +1,32 @@
 package hofbauer.cipher;
 
-public interface Cipher {
+/**
+ * Interface for all Ciphers in hofbauer.cipher
+ * 
+ * @author Manfred Hofbauer
+ *
+ */
+public interface Cipher {	
 	
+	/**
+	 * Given message will be encrypted with this method.
+	 * 
+	 * The methods encrypt and decrypt are reverse operations,
+	 * both can return the same result, if used both with the same String
+	 * 
+	 * @param 	text message to encrypt
+	 * @return 	encrypted message
+	 */
 	public String encrypt(String text);
 	
+	/**
+	 * Given message will be decrypted with this method
+	 * 
+	 * The methods encrypt and decrypt are reverse operations,
+	 * both can return the same result, if used both with the same String
+	 * 
+	 * @param 	text message to decrypt
+	 * @return 	decrypted message
+	 */
 	public String decrypt(String text);
 }
